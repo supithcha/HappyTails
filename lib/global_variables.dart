7ab134ba9b22e3e0fe632e40fa53eec2fe183b47
Russarin <russarin.eai@student.mahidol.edu>
2024-04-06 13:17:00 +0700

    change var

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -1,18 +1,16 @@
 library globals;
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 bool isLoggedIn = false;
 String current_user = '';
-int Current_userID = 0;
-
+int current_userID = 0;
 
 Future<String?> getDocumentIdByUserId(String collectionPath, int userId) async {
   String? documentId;
-
-  // Query Firestore for the document with userID equal to the specified value
   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
       .collection(collectionPath)
-      .where('user_ID', isEqualTo: userId)
+      .where('User_ID', isEqualTo: userId)
       .limit(1)
       .get();
 
@@ -25,135 +23,113 @@ Future<String?> getDocumentIdByUserId(String collectionPath, int userId) async {
 }
 
 Future<int> getTotalDocumentCount(String collectionName) async {
-  // Get a reference to the collection
-  CollectionReference collectionRef = FirebaseFirestore.instance.collection(collectionName);
-
-  // Get the documents in the collection
+  CollectionReference collectionRef =
+      FirebaseFirestore.instance.collection(collectionName);
   QuerySnapshot querySnapshot = await collectionRef.get();
-
-  // Return the total number of documents
   return querySnapshot.size;
 }
 
-
 Future<int?> getUserIdByName(String name) async {
-  int? userId;
-
-  // Query Firestore for the document with name equal to the specified value
+  int? userID;
   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-    .collection('User')
-    .where('User_username', isEqualTo: name)
-    .limit(1)
-    .get();
-
-  // If there is a matching document, retrieve its userID
+      .collection('User')
+      .where('User_username', isEqualTo: name)
+      .limit(1)
+      .get();
   if (querySnapshot.docs.isNotEmpty) {
-    // Parse the 'User_ID' field to an integer
-    userId = int.tryParse(querySnapshot.docs.first.get('User_ID'));
+    userID = int.tryParse(querySnapshot.docs.first.get('User_ID'));
   }
-
-  // Return userId directly
-  return userId;
-}
-
-Future<int?> getUserIdByEmail(String email) async {
-  int? userId;
-
-  // Query Firestore for the document with name equal to the specified value
-  QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-    .collection('User')
-    .where('User_email', isEqualTo: email)
-    .limit(1)
-    .get();
-
-  // If there is a matching document, retrieve its userID
-  if (querySnapshot.docs.isNotEmpty) {
-    // Parse the 'User_ID' field to an integer
-    userId = int.tryParse(querySnapshot.docs.first.get('User_ID'));
-  }
-
-  // Return userId directly
-  return userId;
+  return userID;
 }
 
 Future<String?> getUsernameByID(String id) async {
   String? username;
-
   // Query Firestore for the document with name equal to the specified value
   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-    .collection('User')
-    .where('User_ID', isEqualTo: id)
-    .limit(1)
-    .get();
-
-  // If there is a matching document, retrieve its userID
+      .collection('User')
+      .where('User_ID', isEqualTo: id)
+      .limit(1)
+      .get();
   if (querySnapshot.docs.isNotEmpty) {
-    // Parse the 'User_ID' field to an integer
-    username = querySnapshot.docs.first.get('User_FullName');
+    username = querySnapshot.docs.first.get('User_Fullname');
   }
-
-  // Return userId directly
   return username;
 }
 
-Future<String?> getLocationByID(String id) async {
-  String? location;
+// Future<int?> getUserIdByEmail(String email) async {
+//   int? userId;
+//   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+//       .collection('User')
+//       .where('User_email', isEqualTo: email)
+//       .limit(1)
+//       .get();
+//   // If there is a matching document, retrieve its userID
+//   if (querySnapshot.docs.isNotEmpty) {
+//     // Parse the 'User_ID' field to an integer
+//     userId = int.tryParse(querySnapshot.docs.first.get('User_ID'));
+//   }
+//   // Return userId directly
+//   return userId;
+// }
 
-  // Query Firestore for the document with name equal to the specified value
-  QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-    .collection('User')
-    .where('User_ID', isEqualTo: id)
-    .limit(1)
-    .get();
+// Future<String?> getLocationByID(String id) async {
+//   String? location;
 
-  // If there is a matching document, retrieve its userID
-  if (querySnapshot.docs.isNotEmpty) {
-    // Parse the 'User_ID' field to an integer
-    location = querySnapshot.docs.first.get('User_Location');
-  }
+//   // Query Firestore for the document with name equal to the specified value
+//   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+//       .collection('User')
+//       .where('User_ID', isEqualTo: id)
+//       .limit(1)
+//       .get();
 
-  // Return userId directly
-  return location;
-}
+//   // If there is a matching document, retrieve its userID
+//   if (querySnapshot.docs.isNotEmpty) {
+//     // Parse the 'User_ID' field to an integer
+//     location = querySnapshot.docs.first.get('User_Location');
+//   }
 
-Future<double?> getLatitudeByPostID(String id) async {
-  double? Latitude;
+//   // Return userId directly
+//   return location;
+// }
 
-  // Query Firestore for the document with name equal to the specified value
-  QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-    .collection('Post')
-    .where('Post_ID', isEqualTo: id)
-    .limit(1)
-    .get();
+// Future<double?> getLatitudeByPostID(String id) async {
+//   double? Latitude;
 
-  // If there is a matching document, retrieve its userID
-  if (querySnapshot.docs.isNotEmpty) {
-    // Parse the 'User_ID' field to an integer
-    var temp = querySnapshot.docs.first.get('Latitude');
-    Latitude = double.parse(temp);
-  }
+//   // Query Firestore for the document with name equal to the specified value
+//   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+//       .collection('Post')
+//       .where('Post_ID', isEqualTo: id)
+//       .limit(1)
+//       .get();
 
-  // Return userId directly
-  return Latitude;
-}
+//   // If there is a matching document, retrieve its userID
+//   if (querySnapshot.docs.isNotEmpty) {
+//     // Parse the 'User_ID' field to an integer
+//     var temp = querySnapshot.docs.first.get('Latitude');
+//     Latitude = double.parse(temp);
+//   }
 
-Future<double?> getLongitudeByPostID(String id) async {
-  double? Longtitude;
+//   // Return userId directly
+//   return Latitude;
+// }
 
-  // Query Firestore for the document with name equal to the specified value
-  QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-    .collection('Post')
-    .where('Post_ID', isEqualTo: id)
-    .limit(1)
-    .get();
+// Future<double?> getLongitudeByPostID(String id) async {
+//   double? Longtitude;
 
-  // If there is a matching document, retrieve its userID
-  if (querySnapshot.docs.isNotEmpty) {
-    // Parse the 'User_ID' field to an integer
-    var temp = querySnapshot.docs.first.get('Longtitude');
-    Longtitude = double.parse(temp);
-  }
+//   // Query Firestore for the document with name equal to the specified value
+//   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+//       .collection('Post')
+//       .where('Post_ID', isEqualTo: id)
+//       .limit(1)
+//       .get();
 
-  // Return userId directly
-  return Longtitude;
-}
+//   // If there is a matching document, retrieve its userID
+//   if (querySnapshot.docs.isNotEmpty) {
+//     // Parse the 'User_ID' field to an integer
+//     var temp = querySnapshot.docs.first.get('Longtitude');
+//     Longtitude = double.parse(temp);
+//   }
+
+//   // Return userId directly
+//   return Longtitude;
+// }
